0cb57bc0e599bc5ffb7e7839836343f59d1308d7
ImFawker <tungvumanh2001@gmail.com>
2020-10-15 17:12:26 +0700

    add

---
+++ Session-1/Homework/hwork1/index.js
@@ -0,0 +1,6 @@
+console.log('Invalid name examples are 12a, tung-vu, let, return, class (reserved names)');
+
+console.log('to check data type, use typeof');
+
+console.log(typeof 0);
+console.log(typeof true);
