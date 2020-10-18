4ad24834fcb6b76c937571055b0574294aaeb324
ImFawker <tungvumanh2001@gmail.com>
2020-10-18 21:12:31 +0700

    add

---
+++ Session-2/Homework/hwork11_12/index.js
@@ -0,0 +1,25 @@
+for (var i=1; i <= 4; i++) {
+  fd(50)
+  lt(90)
+}
+
+for (var i=1; i <= 3; i++) {
+  fd(50)
+  lt(120)
+}
+
+for (var i=1; i <= 5; i++) {
+  fd(50)
+  lt(72)
+}  
+  
+for (var i=1; i <= 6; i++) {
+  fd(50)
+  lt(60)
+}
+
+let n = Number(prompt('enter n'));
+for (var i=1; i <= n; i++) {
+    fd(50)
+    lt(360/n)
+}
